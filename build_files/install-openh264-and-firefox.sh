7a2b453f3772e285d2126057eb383681f7c923eb
butterflysky <bright.art1794@fastmail.com>
2025-12-22 18:16:01 -0800

    wtf no mkdir -p /root/blah bc /root exists stupid symlink behavior fools

--- build_files/install-openh264-and-firefox.sh
+++ build_files/install-openh264-and-firefox.sh
@@ -6,36 +6,32 @@ echo "Building OpenH264 from source..."
 # 1. Install build dependencies
 dnf5 install -y git rpmdevtools gcc gcc-c++ make nasm
 
-# 2. Setup the RPM build tree manually
-# Standard tools expect this structure at %{_topdir} (defaults to ~/rpmbuild)
-mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+# 2. Setup the RPM build tree in /var/tmp (Safe from OSTree symlink weirdness)
+# We avoid ~/rpmbuild because /root is a symlink and mkdir -p hates it.
+RPM_ROOT="/var/tmp/rpmbuild"
+mkdir -p "$RPM_ROOT"/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
 # 3. Clone the Fedora 43 spec file
-# We clone into a temp folder, then move the spec to the standard location
 TEMP_CLONE=$(mktemp -d)
 git clone -b f43 https://src.fedoraproject.org/rpms/openh264.git "$TEMP_CLONE"
-cp "$TEMP_CLONE/openh264.spec" ~/rpmbuild/SPECS/
+cp "$TEMP_CLONE/openh264.spec" "$RPM_ROOT/SPECS/"
 rm -rf "$TEMP_CLONE"
 
 # 4. Download sources
-# -g: Get sources
-# -C: Download target directory (Must be the SOURCES dir we just created)
-spectool -g -R ~/rpmbuild/SPECS/openh264.spec -C ~/rpmbuild/SOURCES/
+# Point spectool to our custom /var/tmp directories
+spectool -g -R "$RPM_ROOT/SPECS/openh264.spec" -C "$RPM_ROOT/SOURCES/"
 
 # 5. Install build dependencies
-# This reads the spec and installs libraries needed for compilation
-dnf5 builddep -y ~/rpmbuild/SPECS/openh264.spec
+dnf5 builddep -y "$RPM_ROOT/SPECS/openh264.spec"
 
 # 6. Build the RPM
-# We don't need fancy defines now because we are using the standard ~/rpmbuild structure
-rpmbuild -bb ~/rpmbuild/SPECS/openh264.spec
+# Explicitly define the topdir to our safe location
+rpmbuild -bb "$RPM_ROOT/SPECS/openh264.spec" --define "_topdir $RPM_ROOT"
 
 # 7. Install the resulting RPMs
-dnf5 install -y ~/rpmbuild/RPMS/x86_64/*.rpm
+dnf5 install -y "$RPM_ROOT/RPMS/x86_64/"*.rpm
 
-# Cleanup the build tree to save space in the final image
-rm -rf ~/rpmbuild
-#
+# Cleanup
+rm -rf "$RPM_ROOT"
 # ----------------------------------------
-# NOW you can install Firefox
 dnf5 install -y firefox
